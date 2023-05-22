c661c6fdda1c1b9f11c17555c1828b951a568862
brice <prodakil@gmail.com>
2023-05-22 12:09:40 +0200

    ⚡️ cors / body-parser

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
+import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
-import session from 'express-session';
-import router from "./router/index.js";
-import * as dotenv from 'dotenv'
+import router from "./routes/routes.js";
+import route from "./routes/routes.js";
+import bodyParser from "body-parser";
 import("./config/db.js");
+import cors from "cors";
 
-dotenv.config()
-//Démarrage express
+dotenv.config({ path: "config/.env" });
+
+const { APP_HOSTNAME, APP_PORT, NODE_ENV, FRONT_END_URL } = process.env;
+
 const app = express();
-const port = 3000;
 
-app.use(express.static("public"));
+const corsOptions = {
+  origin: FRONT_END_URL,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.set("view engine", "pug");
+app.locals.pretty = NODE_ENV !== "production"; // Indente correctement le HTML envoyé au client (utile en dev, mais inutile en production)
 
-// récupérer les données post sous forme d'un JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(
-    session({
-        name: "login",
-        secret: "alklkdin987hdjd",
-        resave: true,
-        saveUninitialized: true,
-    })
-);
+app.use(router);
+app.use("/", route);
 
-app.use(function (req, res, next) {
-    app.locals = {
-        title: "Login",
-        isAuth: req.session?.auth ? req.session.auth : false
-    };
-    next();
-});
-
-app.use("/", router);
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(APP_PORT, () => {
+  console.log(`App listening at http://${APP_HOSTNAME}:${APP_PORT}`);
 });
