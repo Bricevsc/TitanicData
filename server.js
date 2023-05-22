fa58d9e9597ba7080cb331237dc7e12bcfc14247
brice <prodakil@gmail.com>
2023-05-22 12:20:36 +0200

    ✅

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import express from "express";
 import router from "./routes/routes.js";
 import route from "./routes/routes.js";
-import bodyParser from "body-parser";
 import("./config/db.js");
 import cors from "cors";
 
@@ -23,7 +22,7 @@ app.use(cors(corsOptions));
 
 app.locals.pretty = NODE_ENV !== "production"; // Indente correctement le HTML envoyé au client (utile en dev, mais inutile en production)
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
