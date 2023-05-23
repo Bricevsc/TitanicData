470a3d0a4eb47764d5f55ed2a8872a04eae86e93
D-Savel <dad.savel@gmail.com>
2023-05-23 16:47:22 +0200

    update home-login / register controllers

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,33 @@
+import { response } from "express";
+import { UserModel } from "../model/User.js";
+import argon2 from "argon2";
+
+
 export default async function (req, res) {
-    res.render("login");
+    const { email, password } = req.body;
+    let errorMessages = []
+
+    if (email.trim() === "" || password.trim() === "") {
+        if (email.trim() === "")
+            errorMessages.push('Email required !');
+        if (password.trim() === "")
+            errorMessages.push('Password required !');
+        res.send({ errorMessages: errorMessages });
+        return
+    }
+
+
+    try {
+        const user = await UserModel.findOne({ email })
+        const passwordVerify = await argon2.verify(user.password, password)
+        if (passwordVerify) {
+            req.session.auth = true;
+            res.status(201).send({ ok: true, user })
+        } else {
+            errorMessages.push(`Invalid email or password`);
+            res.send({ errorMessages: errorMessages })
+        }
+    } catch (err) {
+        res.status(500).json({ status: 'error', message: err.message })
+    }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,13 @@ import express from "express";
 import homeController from "../controllers/home.js";
 import registerController from "../controllers/register.js";
 import dashboardController from "../controllers/dashboard.js"
-import authController from "../controllers/auth.js";
-import checkAuth from "../middlewares/checkauth.js";
+import checkAuth from "../middlewares/checkAuth.js";
 
 const router = express.Router();
 
-router.get("/", homeController);
+router.post("/", homeController);
 router.get("/dashboard", checkAuth, dashboardController);
 router.post("/register", registerController);
-router.post("/auth", authController);
+
 
 export default router;
