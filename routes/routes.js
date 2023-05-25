925150f28ba5d393e704fe5252b34df658bc02a8
D-Savel <dad.savel@gmail.com>
2023-05-25 11:38:20 +0200

    Add validation form middleware for login & register / Add isAuth route

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,20 @@
 import express from "express";
-import homeController from "../controllers/home.js";
+import login from "../controllers/home.js";
 import registerController from "../controllers/register.js";
 import dashboardController from "../controllers/dashboard.js"
+import sessionDestroy from "../controllers/sessionDestroy.js";
+import isAuth from "../controllers/isAuth.js"
 import checkAuth from "../middlewares/checkAuth.js";
-
+import validateLoginPayload from "../middlewares/validateLoginPayload.js"
+import validateRegisterPayload from "../middlewares/validateRegisterPayload.js";
 const router = express.Router();
 
-router.post("/", homeController);
+router.post("/", validateLoginPayload, login);
 router.get("/dashboard", checkAuth, dashboardController);
-router.post("/register", registerController);
+router.post("/register", validateRegisterPayload, registerController);
+router.get("/sessionDestroy", sessionDestroy)
+router.get("/isAuth", isAuth)
+
 
 
 export default router;
