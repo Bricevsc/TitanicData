4a658203f3d5503db63033ff6e895d33fe0e11e9
D-Savel <dad.savel@gmail.com>
2023-05-25 16:49:55 +0200

    update dashboard controller / del sessionDestroy

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,29 +1,16 @@
 import { PassengerModel } from "../model/Passengers.js";
 
 export default async function (req, res) {
-  let data = [];
 
-  const params = {
-    Sex: req.query.sex,
-    Pclass: req.query.class,
-    Age: req.query.age,
-  }
-
-  const asArray = Object.entries(params)
-  const filtered = asArray.filter(([key, value]) => value);
+  const { sex, pclass, age } = req.body
 
   const passengers = await PassengerModel.find();
 
-  // filtre sur la base des passagers vivant et mort 
-  filtered.map((elemSearch) => {
-    passengers = passengers.filter(elem => elem[elemSearch[0]] == elemSearch[1]);
-  });
 
-  const passengersAlive = passengers.filter(elem => elem.Survived === 1);
-  const passengersDead = passengers.filter(elem => elem.Survived === 0);
-  data.push(passengers.length - passengersAlive.length - passengersDead.length, passengersAlive.length, passengersDead.length);
+  const survivedPassengers = await PassengerModel.count()
+  const deadPassengers = await PassengerModel.count()
 
 
-  res.send({ data })
+  res.send({ survivedPassengers, deadPassengers })
 
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import login from "../controllers/home.js";
 import registerController from "../controllers/register.js";
 import dashboardController from "../controllers/dashboard.js"
-import sessionDestroy from "../controllers/sessionDestroy.js";
 import validateLoginPayload from "../middlewares/validateLoginPayload.js"
 import validateRegisterPayload from "../middlewares/validateRegisterPayload.js";
 const router = express.Router();
@@ -10,7 +9,7 @@ const router = express.Router();
 router.post("/", validateLoginPayload, login);
 router.get("/dashboard", dashboardController);
 router.post("/register", validateRegisterPayload, registerController);
-router.get("/sessionDestroy", sessionDestroy)
+
 
 
 
