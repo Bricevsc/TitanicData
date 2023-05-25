a125e0ad5096eb6ef9eb00f0477bd262d6be648c
D-Savel <dad.savel@gmail.com>
2023-05-25 15:34:23 +0200

    debug router

--- routes/routes.js
+++ routes/routes.js
@@ -3,17 +3,15 @@ import login from "../controllers/home.js";
 import registerController from "../controllers/register.js";
 import dashboardController from "../controllers/dashboard.js"
 import sessionDestroy from "../controllers/sessionDestroy.js";
-import isAuth from "../controllers/isAuth.js"
-import checkAuth from "../middlewares/checkAuth.js";
 import validateLoginPayload from "../middlewares/validateLoginPayload.js"
 import validateRegisterPayload from "../middlewares/validateRegisterPayload.js";
 const router = express.Router();
 
 router.post("/", validateLoginPayload, login);
-router.get("/dashboard", checkAuth, dashboardController);
+router.get("/dashboard", dashboardController);
 router.post("/register", validateRegisterPayload, registerController);
 router.get("/sessionDestroy", sessionDestroy)
-router.get("/isAuth", isAuth)
+
 
 
 
