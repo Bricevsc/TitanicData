f25e61cf72b0b9c1080a88809347a6d0acc04a32
D-Savel <dad.savel@gmail.com>
2023-05-26 11:06:44 +0200

    add auth middleware on dashboard

--- routes/routes.js
+++ routes/routes.js
@@ -4,14 +4,11 @@ import registerController from "../controllers/register.js";
 import dashboardController from "../controllers/dashboard.js"
 import validateLoginPayload from "../middlewares/validateLoginPayload.js"
 import validateRegisterPayload from "../middlewares/validateRegisterPayload.js";
+import auth from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/", validateLoginPayload, login);
-router.post("/dashboard", dashboardController);
+router.post("/dashboard", auth, dashboardController);
 router.post("/register", validateRegisterPayload, registerController);
-
-
-
-
 
 export default router;
